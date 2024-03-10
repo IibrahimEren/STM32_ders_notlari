7946d025fb9e94536fcb59a9523fc5bfd035a69c
İbrahim <87008174+IibrahimEren@users.noreply.github.com>
2024-03-10 19:15:58 +0300

    Update Buton_kullanımı.c

--- Buton_kullanımı.c
+++ Buton_kullanımı.c
@@ -1,4 +1,10 @@
-while (1)
+/* USER CODE BEGIN 2 */
+  int32_t delayStats = 250;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
   {
     /* USER CODE END WHILE */
     MX_USB_HOST_Process();
@@ -25,10 +31,18 @@ while (1)
     HAL_Delay(delayStats);
     HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, 0);
     HAL_Delay(delayStats);
+
+    delayStats = delayStats - (20);
+    if(delayStats <= 0){
+    	delayStats = 250;
+    }
     //Toogle bir yandırıp bir söndürmeye yarıyor
     //HAL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
     //HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
     //Mavi buton basılı tutuldukça ledlerin yanıp sönmesini sağlayan bir kod yaptık
+    /*Burada yapılan kod akarken mavi butona basılıp basılmadığını kontrol ederek basıldığı taktirde bu if bloğunun
+     * içerisine girmesini sağlayan bir kod yaptık ---> Bu dış kesmeler kullanılarak yapılması daha uygun olacaktır.
+    */
     if(HAL_GPIO_ReadPin(blueButton_GPIO_Port, blueButton_Pin)){
 	    HAL_Delay(delayStats);
 
